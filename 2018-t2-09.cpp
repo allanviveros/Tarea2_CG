fc0e609d520ea1a3cca4a366eedefc0be86a21e7
allan <allan.viveros.14@sansano.usm.cl>
2018-11-04 19:01:59 -0300

    se agrego el traslado del rectangulo

--- 2018-t2-09.cpp
+++ 2018-t2-09.cpp
@@ -8,9 +8,17 @@
 // Numero de Grupo: 9
 
 #include <GL/glut.h>
+#include "iostream"
+using namespace std;
+
+float speedMoveRectangle = 0;
 
 void init();
 void mydisplay();
+//Rectangle
+void TimeMoveRectangle(int time);
+void CreateRectangle();
+void MoveRectangle();
 
 int main(int argc, char** argv) {
 	glutInit(&argc, argv);
@@ -19,32 +27,23 @@ int main(int argc, char** argv) {
 	glutInitWindowPosition(0, 0);
 	glutCreateWindow("Tarea 2, Computación Gráfica: Juan Pablo León y Allan Viveros");
 	glutDisplayFunc(mydisplay);
-
 	init();
-
+	glutTimerFunc(0, TimeMoveRectangle, 10);
 	glutMainLoop();
-
 }
 
 void init() {
 	glClearColor(1.0, 1.0, 1.0, 1.0); //color de fondo
-	glMatrixMode(GL_PROJECTION);
+	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
 	glOrtho(-1.0, 1.0, -1.0, 1.0, -1.0, 1.0);
 }
 
-void mydisplay()
-{
+void mydisplay(){
 	glClear(GL_COLOR_BUFFER_BIT);
 
-	glColor3f(0.0, 0.0, 1.0);
-	glBegin(GL_POLYGON);
-	glVertex2f(-0.9, 0.9);
-	glVertex2f(0.9, 0.9);
-	glVertex2f(0.9, 0.3);
-	glVertex2f(-0.9, 0.3);
-	glEnd();
-
+	MoveRectangle();
+	
 	glColor3f(0.0, 1.0, 0.0);
 	glBegin(GL_POLYGON);
 	glVertex2f(0.0, 0.0);
@@ -53,11 +52,43 @@ void mydisplay()
 	glVertex2f(0.0, -0.9);
 	glEnd();
 
+
 	glColor3f(1.0, 0.0, 0);
 	glBegin(GL_TRIANGLES);
 	glVertex2f(-0.55, 0.0);
 	glVertex2f(-0.2, -0.9);
 	glVertex2f(-0.9, -0.9);
 	glEnd();
+
 	glFlush();
 }
+
+//Rectangle
+
+void CreateRectangle() {
+	glColor3f(0.0, 0.0, 1.0);
+	glBegin(GL_POLYGON);
+	glVertex2f(-0.9, 0.9);
+	glVertex2f(0.9, 0.9);
+	glVertex2f(0.9, 0.3);
+	glVertex2f(-0.9, 0.3);
+	glEnd();
+}
+
+void TimeMoveRectangle(int time) {
+	speedMoveRectangle += 0.003;
+	if (speedMoveRectangle > 2) {
+		speedMoveRectangle = -2;
+	}
+	glutTimerFunc(time, TimeMoveRectangle, time);
+	glutPostRedisplay();
+}
+
+void MoveRectangle() {
+	glPushMatrix();
+	glTranslatef(speedMoveRectangle, 0.0, 0.0);
+	CreateRectangle();
+	glPopMatrix();
+}
+
+
